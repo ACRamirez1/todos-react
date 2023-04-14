d2c7ae8f683dcdcff7008ac529dfbc41ed426fac
ACRamirez1 <caramirez711@gmail.com>
2023-04-13 23:34:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 
-function App() {
-  return (
+//I want to use reacts component module
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {    //keeps track of data
+// keep track of "state" statements. You need to keep up with how you use them
+// and how they are changed. 
+      isClicked: false,
+      todos: [{'id':1, 'text': "Walk the fish"}, {'id':2, 'text': "Watch a movie"}],
+      text: "",
+    };
+
+    
+    }
+handleClick = () => {
+      console.log("Clicked", this)
+      this.setState({
+
+        isClicked: !this.state.isClicked
+
+      });
+  }
+
+  handleChange = (event) => {
+
+    this.setState({
+      text: event.target.value
+    })
+    }
+
+    handleSubmit = () => {
+      this.setState({
+        // todos: [this.state.text]
+
+        // use spread to say get all the existing todos first 
+        // and then add the submitted todo
+        // this is called dynamic rendering
+        todos: [...this.state.todos, {id: nanoid(), text: this.state.text}],
+
+        //to get the text to clear, you need an empty string.
+        text: "",
+
+      })
+    }
+
+
+    handleDelete = (id) => {
+
+      console.log("Id is: ", id)
+      const index = this.state.todos.findIndex(todo => todo.id === id)
+      console.log("index", index)
+
+      const copy = [...this.state.todos]
+
+      copy.splice(index, 1)
+
+      this.setState({
+        todos: copy
+      })
+
+
+    }
+
+
+
+ 
+render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type = "text" onChange = {this.handleChange} value = {this.state.text} />
+
+      <button onClick = {this.handleSubmit}>Add Todo</button>
+
+      <ul>
+
+        {this.state.todos.map( (todo) => {
+
+          return <li className="todo-item" key ={todo.id}>
+            <h4>{todo.text}</h4>
+          <button onClick={() => this.handleDelete(todo.id)}>X</button>
+        
+        </li>
+
+        })}
+
+      </ul>
+      
+      <div className='divider'></div>
+      <h4>{this.state.isClicked === true ? "Clicked" : "Not Clicked"}</h4>
+      <button onClick={this.handleClick}>Click Me</button>
     </div>
-  );
+  )
 }
+ }
 
 export default App;
